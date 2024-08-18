f18e185086823f992479e0ec90dcafc193d4ab33
NGOM-cheikh <mourtalla211@gmail.com>
2024-08-18 12:22:09 +0200

    Initial commit

---
+++ src/main/java/fr/diginamic/demofirstspring/DemoFirstSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.demofirstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/demofirstspring/DemoFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.demofirstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
